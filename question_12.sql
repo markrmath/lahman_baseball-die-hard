9ddfe1a00e7ce45b9ca65a7141a5fa4a42c1768c
William Prunty <william.m.prunty@gmail.com>
2023-02-15 12:36:19 -0600

    initial push

---
+++ question_12.sql
@@ -0,0 +1,12 @@
+--12. In this question, you will explore the connection between number of wins and attendance.
+    
+      --a. Does there appear to be any correlation between attendance at home games and number of wins? </li>
+      
+	  
+	  
+	  
+	  
+	  --b. Do teams that win the world series see a boost in attendance the following year? What about teams that made the playoffs? Making the playoffs means either being a division winner or a wild card winner.
+   
+   
+   
